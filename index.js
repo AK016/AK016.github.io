226d77b6c57993add187c3a8693ea60c5f6f6b92
Akshay Kadam <123861375+AK016@users.noreply.github.com>
2023-07-10 20:16:20 +0530

    Resume download functionality

--- index.js
+++ index.js
@@ -61,3 +61,11 @@ function scrollProjects(direction) {
 
   projectContainer.style.transform = `translateX(-${scrollPosition}px)`;
 }
+
+
+const resumeLink = document.querySelector('.nav-link.resume');
+
+resumeLink.addEventListener('click', function(event) {
+  event.preventDefault();
+  window.open(this.href, '_blank', 'noopener');
+});
