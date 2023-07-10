59c4d5ec2ad3de245fee2abdae7efe5fdd472a99
Akshay Kadam <123861375+AK016@users.noreply.github.com>
2023-07-11 00:26:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -78,5 +78,57 @@ function scrollProjects(direction) {
   projectContainer.style.transform = `translateX(-${scrollPosition}px)`;
 }
 
+const jobTitleElement = document.getElementById('job-title');
+const cursorElement = document.createElement('span');
+cursorElement.classList.add('cursor');
+cursorElement.textContent = '|';
+jobTitleElement.appendChild(cursorElement);
+
+const jobTitles = [
+  'Full Stack Web Developer.',
+  'Java Backend Developer.'
+];
+
+let currentIndex = 0;
+
+function updateJobTitle() {
+  const currentJobTitle = jobTitles[currentIndex];
+  const typingDelay = 150; // Delay between typing each character
+  const erasingDelay = 100; // Delay before erasing the job title
+
+  let i = 0;
+  let isErasing = false;
+
+  function typeNextCharacter() {
+    if (isErasing) {
+      const updatedJobTitle = currentJobTitle.slice(0, i);
+      jobTitleElement.textContent = updatedJobTitle;
+      i--;
+      if (i === 0) {
+        isErasing = false;
+        currentIndex = (currentIndex + 1) % jobTitles.length;
+        setTimeout(updateJobTitle, 500);
+      } else {
+        setTimeout(typeNextCharacter, erasingDelay);
+      }
+    } else {
+      const updatedJobTitle = currentJobTitle.slice(0, i) + '|';
+      jobTitleElement.textContent = updatedJobTitle;
+      i++;
+      if (i <= currentJobTitle.length) {
+        setTimeout(typeNextCharacter, typingDelay);
+      } else {
+        isErasing = true;
+        cursorElement.style.visibility = 'hidden'; // Hide cursor when erasing
+        setTimeout(typeNextCharacter, erasingDelay);
+      }
+    }
+  }
+
+  typeNextCharacter();
+}
+
+// Start the job title animation
+updateJobTitle();
 
 
