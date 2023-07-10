3041a97afbaf4e3c23ee0821491eb91be71c498b
Akshay Kadam <123861375+AK016@users.noreply.github.com>
2023-07-11 03:15:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 // Smooth scroll to section
 function smoothScroll(target, duration) {
-    const targetSection = document.querySelector(target);
-    const targetPosition = targetSection.getBoundingClientRect().top;
-    const startPosition = window.pageYOffset;
-    const distance = targetPosition - startPosition;
-    let startTime = null;
-  
-    function scrollAnimation(currentTime) {
-      if (startTime === null) startTime = currentTime;
-      const timeElapsed = currentTime - startTime;
-      const scrollY = ease(timeElapsed, startPosition, distance, duration);
-      window.scrollTo(0, scrollY);
-      if (timeElapsed < duration) requestAnimationFrame(scrollAnimation);
-    }
-  
-    function ease(t, b, c, d) {
-      t /= d / 2;
-      if (t < 1) return c / 2 * t * t + b;
-      t--;
-      return -c / 2 * (t * (t - 2) - 1) + b;
-    }
-  
-    requestAnimationFrame(scrollAnimation);
-  }
-  
-  // Get all the navigation links
-const navLinks = document.querySelectorAll('#nav-menu a');
+  const targetSection = document.querySelector(target);
+  const targetPosition = targetSection.getBoundingClientRect().top;
+  const startPosition = window.pageYOffset;
+  const distance = targetPosition - startPosition;
+  let startTime = null;
 
+  function scrollAnimation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    const timeElapsed = currentTime - startTime;
+    const scrollY = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, scrollY);
+    if (timeElapsed < duration) requestAnimationFrame(scrollAnimation);
+  }
+
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(scrollAnimation);
+}
+
+
+
+  
 // Add event listener to track scroll position
 window.addEventListener('scroll', function() {
   const currentScroll = window.pageYOffset;
