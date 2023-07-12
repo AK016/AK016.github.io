b4e4e3a0f2f40f4e81b8de4f255d32011a89457d
Akshay Kadam <123861375+AK016@users.noreply.github.com>
2023-07-12 23:25:51 +0530

    Dark Mode Enabled

--- index.js
+++ index.js
@@ -211,3 +211,35 @@ function handleScroll() {
 // Event listener for scroll event
 window.addEventListener('scroll', handleScroll);
 
+
+
+
+
+// dark mode toggle 
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('change', function () {
+  if (darkModeToggle.checked) {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
+
+function enableDarkMode() {
+  body.classList.add('dark-mode');
+  localStorage.setItem('darkModeEnabled', true);
+}
+
+function disableDarkMode() {
+  body.classList.remove('dark-mode');
+  localStorage.setItem('darkModeEnabled', false);
+}
+
+// Check if dark mode was previously enabled
+const darkModeEnabled = localStorage.getItem('darkModeEnabled');
+
+if (darkModeEnabled && darkModeEnabled === 'true') {
+  enableDarkMode();
+}
